a5ec5aa6a73fd7a39871a68f8009b75a99de6a65
gabrieltoninatto <gabrieltoninatto1@hotmail.com>
2025-09-23 16:18:03 -0300

    refatora a função main para usar goroutines e WaitGroup na busca de preços

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,35 @@ package main
 import (
 	"buscador/internal/fetcher"
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
 	start := time.Now()
-	price1 := fetcher.FetchPriceFromSite1()
-	price2 := fetcher.FetchPriceFromSite2()
-	price3 := fetcher.FetchPriceFromSite3()
+	var price1, price2, price3 float64
+
+	//WaitGrup (grupo de espera)
+	var wg sync.WaitGroup
+	wg.Add(3)
+
+	//Goroutines: funções ou métodos executados de forma concorrente.
+	go func() {
+		defer wg.Done()
+		price1 = fetcher.FetchPriceFromSite1()
+	}()
+
+	go func() {
+		defer wg.Done()
+		price2 = fetcher.FetchPriceFromSite2()
+	}()
+
+	go func() {
+		defer wg.Done()
+		price3 = fetcher.FetchPriceFromSite3()
+	}()
+
+	wg.Wait()
 
 	fmt.Printf("R$%.2f \n", price1)
 	fmt.Printf("R$%.2f \n", price2)
