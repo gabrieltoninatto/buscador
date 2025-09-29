534606465ee42e6d63b11c8c2aa4f7b738afe33a
gabrieltoninatto <gabrieltoninatto1@hotmail.com>
2025-09-29 16:39:39 -0300

    refatora a função FetchPrices para gerenciar a busca de preços com goroutines e WaitGroup

--- cmd/main.go
+++ cmd/main.go
@@ -13,8 +13,7 @@ func main() {
 	priceChannel := make(chan float64)
 
 	//WaitGroup (grupo de espera)
-	var wg, showWg sync.WaitGroup
-	wg.Add(3)
+	var showWg sync.WaitGroup
 	showWg.Add(1)
 
 	//Goroutines: funções ou métodos executados de forma concorrente.
@@ -24,23 +23,8 @@ func main() {
 		processor.ShowPriceAVG(priceChannel)
 	}()
 
-	go func() {
-		defer wg.Done()
-		priceChannel <- fetcher.FetchPriceFromSite1()
-	}()
+	go fetcher.FetchPrices(priceChannel)
 
-	go func() {
-		defer wg.Done()
-		priceChannel <- fetcher.FetchPriceFromSite2()
-	}()
-
-	go func() {
-		defer wg.Done()
-		priceChannel <- fetcher.FetchPriceFromSite3()
-	}()
-
-	wg.Wait()
-	close(priceChannel)
 	showWg.Wait()
 
 	fmt.Printf("\nTempo total: %s", time.Since(start))
--- internal/fetcher/price_fetcher.go
+++ internal/fetcher/price_fetcher.go
@@ -2,11 +2,34 @@ package fetcher
 
 import (
 	"math/rand/v2"
+	"sync"
 	"time"
 )
 
-//Buscar preços de diferentes sites
+// chan<- envia informações para o canal
+func FetchPrices(priceChannel chan<- float64) {
+	var wg sync.WaitGroup
+	wg.Add(3)
+	go func() {
+		defer wg.Done()
+		priceChannel <- FetchPriceFromSite1()
+	}()
 
+	go func() {
+		defer wg.Done()
+		priceChannel <- FetchPriceFromSite2()
+	}()
+
+	go func() {
+		defer wg.Done()
+		priceChannel <- FetchPriceFromSite3()
+	}()
+
+	wg.Wait()
+	close(priceChannel)
+}
+
+// Buscar preços de diferentes sites
 func FetchPriceFromSite1() float64 {
 	time.Sleep(1 * time.Second)
 	return rand.Float64() * 100
